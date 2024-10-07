065b011f9a8d424970e54069a3a5df0c4d18ecf5
Vitor Palheta <palheta.vitor@gmail.com>
2024-10-07 18:13:33 -0300

    Adicionando item a lista via JS

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-const item = document.getElementById("item");
+const inputBox = document.getElementById("item");
+const listContainer = document.getElementById("list-container");
+
+function addItem(){
+  if(inputBox.value === ''){
+    alert("Você deve escrever alguma coisa!")
+  }
+  else{
+    let li = document.createElement("li")
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let image = document.createElement("img");
+    image.src = 'images/icons/trash.svg'
+
+    
+    li.appendChild(image);
+  }
+  inputBox.value = "";
+}
 
 //Manipulando o input item para receber textos
-item.addEventListener("input", () =>{
-  console.log(item.value)
-})
+// item.addEventListener("input", () =>{
+//   console.log(item.value)
+// })
+
